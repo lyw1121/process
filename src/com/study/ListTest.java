d9db4b1400003136e0ecafda5a739c342c0df364
wangyl <wangyilu1121@163.com>
2019-02-23 14:56:36 +0800

    study

--- src/com/study/ListTest.java
+++ src/com/study/ListTest.java
@@ -21,7 +21,7 @@ public class ListTest {
             System.out.println(arraylist[j]);
         }
         System.out.println("********************************");
-        //创建迭代器 对集合进行遍历输出
+        //创建迭代器 对集合进行遍历输出,创建iterator对象,调用其hasNext()方法查找集合迭代器容器中是否存在下一个元素
         Iterator<String> str = sl.iterator();
         while (str.hasNext()){
             System.out.println(str.next());
@@ -39,6 +39,7 @@ public class ListTest {
         }
         if (set.contains("bb")){
             System.out.println("set集合找到了！");
+            System.out.println(set.contains("cc"));    //contains()方法是返回的是一个boolean类型的结果,即如果在集合中包含该对象则返回结果为true，反之为false
         }
 
 
